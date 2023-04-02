d9db4a6dc9165e2c0b187691ec03f7f8ffc4fa5f
Gabryzzzzz <gabriele.monti24@gmail.com>
2023-04-02 21:38:42 +0200

    added get test with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticateService } from './services/authenticate.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FERememberVR8.0';
 
-  constructor(public _authService: AuthenticateService, public _router: Router) { }
+  constructor(
+    public _authService: AuthenticateService,
+    public _router: Router,
+    public _http: HttpClient) { }
+
+  ngOnInit(): void {
+    this._http.get('https://localhost:7054/Video/GetAllNames').subscribe(x=> console.log(x))
+  }
 
   seeNavBar(): boolean {
     return this._authService.isLogged() && this._router.url != '/login' && this._router.url != '/registration';
